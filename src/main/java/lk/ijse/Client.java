8ec590cf5b741a9d14d7cf37feccdf19dd27449a
bandara <chathubandara500@gmail.com>
2024-01-06 11:31:34 +0530

    Client and Server Connected...!

---
+++ src/main/java/lk/ijse/Client.java
@@ -0,0 +1,23 @@
+package lk.ijse;
+
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Client {
+    public static void main(String[] args) {
+        try {
+            Socket socket = new Socket("localhost",3000);
+            System.out.println("Socket Created....!");
+            DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
+            String message = "Hello Server";
+            dataOutputStream.writeUTF(message);
+            dataOutputStream.close();
+            socket.close();
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ src/main/java/lk/ijse/Server.java
@@ -0,0 +1,25 @@
+package lk.ijse;
+
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+    public static void main(String[] args) {
+        try {
+            ServerSocket serverSocket = new ServerSocket(3000);
+            System.out.println("Server Socket Created...!");
+            Socket socket = serverSocket.accept();
+            DataInputStream dataInputStream = new DataInputStream(socket.getInputStream());
+            String message = dataInputStream.readUTF();
+            System.out.println("Server Received...!");
+            socket.close();
+            serverSocket.close();
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
